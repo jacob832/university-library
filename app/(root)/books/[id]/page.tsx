1f3b8c980e79d427709f29d22959ecc22ede81ca
jacob832 <jacobhasan832@gmail.com>
2025-05-07 20:24:48 +0300

    FINISH!

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -1,50 +1,54 @@
-import React from "react";
+import { eq, ne } from "drizzle-orm";
+import { redirect } from "next/navigation";
+
+import BookList from "@/components/BookList";
+import BookVideo from "@/components/BookVideo";
+import BookOverview from "@/components/BookOverview";
+
+import { auth } from "@/auth";
 import { db } from "@/database/drizzle";
 import { books } from "@/database/schema";
-import { eq } from "drizzle-orm";
-import { redirect } from "next/navigation";
-import { auth } from "@/auth";
-import BookOverview from "@/components/BookOverview";
-import BookVideo from "@/components/BookVideo";
 
-const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+const Page = async ({ params }: PageProps) => {
   const id = (await params).id;
   const session = await auth();
 
-  // Fetch data based on id
-  const [bookDetails] = await db
-    .select()
-    .from(books)
-    .where(eq(books.id, id))
-    .limit(1);
+  const [[book], similarBooks] = await Promise.all([
+    db.select().from(books).where(eq(books.id, id)).limit(1) as Promise<Book[]>,
+    db.select().from(books).where(ne(books.id, id)).limit(4) as Promise<Book[]>,
+  ]);
 
-  if (!bookDetails) redirect("/404");
+  if (!book) redirect("/404");
 
   return (
     <>
-      <BookOverview {...bookDetails} userId={session?.user?.id as string} />
+      <BookOverview {...book} userId={session?.user?.id as string} />
 
       <div className="book-details">
         <div className="flex-[1.5]">
           <section className="flex flex-col gap-7">
             <h3>Video</h3>
-
-            <BookVideo videoUrl={bookDetails.videoUrl} />
+            <BookVideo videoUrl={book.videoUrl} />
           </section>
+
           <section className="mt-10 flex flex-col gap-7">
             <h3>Summary</h3>
-
             <div className="space-y-5 text-xl text-light-100">
-              {bookDetails.summary.split("\n").map((line, i) => (
-                <p key={i}>{line}</p>
-              ))}
+              {book.summary
+                ?.split("\n")
+                .map((line, index) => <p key={index}>{line}</p>)}
             </div>
           </section>
         </div>
 
-        {/*  SIMILAR*/}
+        <section className="sticky top-5 h-fit flex-1">
+          {similarBooks.length > 0 && (
+            <BookList title="More Similar Books" books={similarBooks} />
+          )}
+        </section>
       </div>
     </>
   );
 };
+
 export default Page;
